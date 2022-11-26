caf0e3122f4c7aad8b5456909a7a87a00712955c
Nervnost <leonovsergej647@gmail.com>
2022-11-26 15:12:18 +0300

    добавлена функция bubble (сортировка)

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+def bubble(a):
+    for i in range(0,len(a)-1):
+        for m in range(len(a)-1):
+            if(a[m]>a[m+1]):
+                a[m],a[m+1] = a[m+1], a[m]
+    return a
 import random
 a = []
 for i in range(10):
     a.append(random.randint(1,100))
 print(a)
-a.sort()
-print(a)
-print(a)
+print(bubble(a))
