3d2676f23275da2d5468b83b1eabc83a8e3929f5
Nervnost <leonovsergej647@gmail.com>
2022-11-26 16:42:16 +0300

    четыре внутренних цикла

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-matrix = [[1,2,3], [4,5,6], [7,8,9]]
-print(matrix[2][2])
-x=0
-y=1
-line=matrix[y]
-elem=line[x]
-print(elem)
+n = 9
+matrix = [0] * n
+for i in range(n):
+    matrix[i] = [0] * n
+x = 0
+y = 0
+z = 1
 
+for x in range(n-1):
+    matrix[0][x] = z
+    z +=1
+for y in range(n-1):
+    matrix[y][n-1] = z
+    z +=1
+for x in range(n-1):
+    matrix[n-1][n-1-x] = z
+    z += 1
+for y in range(n-1):
+    matrix[n-1-y][0] = z
+    z += 1
 for a in matrix:
     print(a)
